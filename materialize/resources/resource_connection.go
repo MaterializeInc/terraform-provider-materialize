f2304868d9b3096446df13f88eda25fe7ee9bf2a
Bobby Iliev <bobby@bobbyiliev.com>
2023-02-09 19:21:19 +0200

    Fix read query

--- materialize/resources/resource_connection.go
+++ materialize/resources/resource_connection.go
@@ -32,6 +32,12 @@ func Connection() *schema.Resource {
 				Optional:    true,
 				Default:     "public",
 			},
+			"database_name": {
+				Description: "The identifier for the secret database.",
+				Type:        schema.TypeString,
+				Optional:    true,
+				Default:     "materialize",
+			},
 			"connection_type": {
 				Description:  "The type of the connection.",
 				Type:         schema.TypeString,
@@ -620,10 +626,13 @@ func (b *ConnectionBuilder) Read() string {
 			mz_connections.id,
 			mz_connections.name,
 			mz_connections.type,
+			mz_databases.name AS database_name,
 			mz_schemas.name AS schema_name
 		FROM mz_connections
 		JOIN mz_schemas
 			ON mz_connections.schema_id = mz_schemas.id
+		JOIN mz_databases
+			ON mz_schemas.database_id = mz_databases.id
 		WHERE mz_connections.name = '%s'
 		AND mz_schemas.name = '%s'
 		AND mz_databases.name = '%s';
