0ffc9d07033b175f7ba2e4ac499453c1bfd57630
Dennis Hume <dennis@materialize.com>
2023-02-28 05:53:33 -0600

    Fix Int Tests

--- integration/sink.tf
+++ integration/sink.tf
@@ -11,7 +11,7 @@ resource "materialize_sink" "sink_kafka" {
   envelope                   = "DEBEZIUM"
 
   depends_on = [
-    materialize_source_loadgen.load_generator
+    materialize_source_load_generator.load_generator
   ]
 }
 
