880233bb6f538508e4eea69bf668bdc588bb868d
Dennis Hume <dennis@materialize.com>
2023-03-22 06:41:14 -0500

    Check

--- integration/index.tf
+++ integration/index.tf
@@ -1,5 +1,10 @@
 resource "materialize_index" "loadgen_index" {
   name         = "loadgen_index"
-  obj_name     = materialize_source_load_generator.load_generator_cluster.qualified_name
   cluster_name = materialize_cluster.cluster.name
+
+  obj_name {
+    name          = materialize_source_load_generator.load_generator_cluster.name
+    schema_name   = materialize_connection_kafka.kafka_connection.schema_name
+    database_name = materialize_connection_kafka.kafka_connection.database_name
+  }
 }
--- pkg/materialize/index.go
+++ pkg/materialize/index.go
@@ -97,16 +97,50 @@ func (b *IndexBuilder) ReadId() string {
 
 func ReadIndexParams(id string) string {
 	return fmt.Sprintf(`
-		SELECT 
+		SELECT DISTINCT
 			mz_indexes.name,
-			mz_schemas.name,
-			mz_databases.name
+			COALESCE(sources.o_name, views.o_name, mviews.o_name),
+			COALESCE(sources.s_name, views.s_name, mviews.s_name),
+			COALESCE(sources.d_name, views.d_name, mviews.d_name)
 		FROM mz_indexes
-		JOIN mz_sources
-			ON mz_indexes.on_id = mz_sources.id
-		JOIN mz_schemas
-			ON mz_sources.schema_id = mz_schemas.id
-		JOIN mz_databases
-			ON mz_schemas.database_id = mz_databases.id
+		LEFT JOIN (
+			SELECT
+				mz_sources.id,
+				mz_sources.name AS o_name,
+				mz_schemas.name AS s_name,
+				mz_databases.name AS d_name
+			FROM mz_sources
+			JOIN mz_schemas
+				ON mz_sources.schema_id = mz_schemas.id
+			JOIN mz_databases
+				ON mz_schemas.database_id = mz_databases.id
+		) sources
+			ON mz_indexes.on_id = sources.id
+		LEFT JOIN (
+			SELECT
+				mz_views.id,
+				mz_views.name AS o_name,
+				mz_schemas.name AS s_name,
+				mz_databases.name AS d_name
+			FROM mz_views
+			JOIN mz_schemas
+				ON mz_views.schema_id = mz_schemas.id
+			JOIN mz_databases
+				ON mz_schemas.database_id = mz_databases.id
+		) views
+			ON mz_indexes.on_id = sources.id
+		LEFT JOIN (
+			SELECT
+				mz_materialized_views.id,
+				mz_materialized_views.name AS o_name,
+				mz_schemas.name AS s_name,
+				mz_databases.name AS d_name
+			FROM mz_materialized_views
+			JOIN mz_schemas
+				ON mz_materialized_views.schema_id = mz_schemas.id
+			JOIN mz_databases
+				ON mz_schemas.database_id = mz_databases.id
+		) mviews
+			ON mz_indexes.on_id = sources.id
 		WHERE mz_indexes.id = '%s';`, id)
 }
