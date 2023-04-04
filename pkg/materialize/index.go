0db1de01dd15dafed666c54bd42d79e0dd36e5e9
Dennis Hume <dennis@materialize.com>
2023-04-04 15:24:55 -0500

    Datasource

--- pkg/materialize/index.go
+++ pkg/materialize/index.go
@@ -144,3 +144,67 @@ func ReadIndexParams(id string) string {
 			ON mz_indexes.on_id = sources.id
 		WHERE mz_indexes.id = %s;`, QuoteString(id))
 }
+
+func ReadIndexDatasource(databaseName, schemaName string) string {
+	q := strings.Builder{}
+	q.WriteString(`
+		SELECT DISTINCT
+			mz_indexes.id,
+			mz_indexes.name,
+			COALESCE(sources.o_name, views.o_name, mviews.o_name),
+			COALESCE(sources.s_name, views.s_name, mviews.s_name),
+			COALESCE(sources.d_name, views.d_name, mviews.d_name)
+		FROM mz_indexes
+		LEFT JOIN (
+			SELECT
+				mz_sources.id,
+				mz_sources.name AS o_name,
+				mz_schemas.name AS s_name,
+				mz_databases.name AS d_name
+			FROM mz_sources
+			JOIN mz_schemas
+				ON mz_sources.schema_id = mz_schemas.id
+			JOIN mz_databases
+				ON mz_schemas.database_id = mz_databases.id
+		) sources
+			ON mz_indexes.on_id = sources.id
+		LEFT JOIN (
+			SELECT
+				mz_views.id,
+				mz_views.name AS o_name,
+				mz_schemas.name AS s_name,
+				mz_databases.name AS d_name
+			FROM mz_views
+			JOIN mz_schemas
+				ON mz_views.schema_id = mz_schemas.id
+			JOIN mz_databases
+				ON mz_schemas.database_id = mz_databases.id
+		) views
+			ON mz_indexes.on_id = sources.id
+		LEFT JOIN (
+			SELECT
+				mz_materialized_views.id,
+				mz_materialized_views.name AS o_name,
+				mz_schemas.name AS s_name,
+				mz_databases.name AS d_name
+			FROM mz_materialized_views
+			JOIN mz_schemas
+				ON mz_materialized_views.schema_id = mz_schemas.id
+			JOIN mz_databases
+				ON mz_schemas.database_id = mz_databases.id
+		) mviews
+			ON mz_indexes.on_id = sources.id
+		WHERE COALESCE(sources.o_name, views.o_name, mviews.o_name) IS NOT NULL`)
+
+	if databaseName != "" {
+		q.WriteString(fmt.Sprintf(`
+		AND mz_databases.name = %s`, QuoteString(databaseName)))
+
+		if schemaName != "" {
+			q.WriteString(fmt.Sprintf(` AND mz_schemas.name = %s`, QuoteString(schemaName)))
+		}
+	}
+
+	q.WriteString(`;`)
+	return q.String()
+}
