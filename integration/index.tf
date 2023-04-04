97f587f5599e2536bdab291b6673eeb8d807b71f
Dennis Hume <dennis@materialize.com>
2023-04-04 15:03:04 -0500

    Check

--- integration/index.tf
+++ integration/index.tf
@@ -4,8 +4,8 @@ resource "materialize_index" "loadgen_index" {
 
   obj_name {
     name          = materialize_source_load_generator.load_generator_cluster.name
-    schema_name   = materialize_connection_kafka.kafka_connection.schema_name
-    database_name = materialize_connection_kafka.kafka_connection.database_name
+    schema_name   = materialize_source_load_generator.load_generator_cluster.schema_name
+    database_name = materialize_source_load_generator.load_generator_cluster.database_name
   }
 }
 
