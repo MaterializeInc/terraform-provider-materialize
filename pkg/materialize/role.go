dd0ca34e65320fc1bef9432a1407326b6f5303f4
Dennis Hume <dennis@materialize.com>
2023-04-28 16:44:44 -0500

    Resource Role

---
+++ pkg/materialize/role.go
@@ -0,0 +1,107 @@
+package materialize
+
+import (
+	"fmt"
+	"strings"
+)
+
+type RoleBuilder struct {
+	roleName      string
+	inherit       bool
+	createRole    bool
+	createDb      bool
+	createCluster bool
+}
+
+func (b *RoleBuilder) QualifiedName() string {
+	return QualifiedName(b.roleName)
+}
+
+func NewRoleBuilder(roleName string) *RoleBuilder {
+	return &RoleBuilder{
+		roleName: roleName,
+	}
+}
+
+func (b *RoleBuilder) Inherit() *RoleBuilder {
+	b.inherit = true
+	return b
+}
+
+func (b *RoleBuilder) CreateRole() *RoleBuilder {
+	b.createRole = true
+	return b
+}
+
+func (b *RoleBuilder) CreateDb() *RoleBuilder {
+	b.createDb = true
+	return b
+}
+
+func (b *RoleBuilder) CreateCluster() *RoleBuilder {
+	b.createCluster = true
+	return b
+}
+
+func (b *RoleBuilder) Create() string {
+	q := strings.Builder{}
+	q.WriteString(fmt.Sprintf(`CREATE ROLE %s`, b.QualifiedName()))
+
+	var p []string
+
+	if b.inherit {
+		p = append(p, `INHERIT`)
+	}
+
+	if b.createRole {
+		p = append(p, `CREATEROLE`)
+	}
+
+	if b.createDb {
+		p = append(p, `CREATEDB`)
+	}
+
+	if b.createCluster {
+		p = append(p, `CREATECLUSTER`)
+	}
+
+	if len(p) > 0 {
+		f := strings.Join(p, " ")
+		q.WriteString(fmt.Sprintf(` %s`, f))
+	}
+
+	q.WriteString(`;`)
+	return q.String()
+}
+
+func (b *RoleBuilder) Alter(p string) string {
+	return fmt.Sprintf(`ALTER ROLE %s %s;`, b.QualifiedName(), p)
+}
+
+func (b *RoleBuilder) Drop() string {
+	return fmt.Sprintf(`DROP ROLE %s;`, b.QualifiedName())
+}
+
+func (b *RoleBuilder) ReadId() string {
+	return fmt.Sprintf(`
+		SELECT id
+		FROM mz_roles
+		WHERE name = %s`, QuoteString(b.roleName))
+}
+
+func ReadRoleParams(id string) string {
+	return fmt.Sprintf(`
+		SELECT
+			id,
+			name,
+			inherit,
+			create_role,
+			create_db,
+			create_cluster
+		FROM mz_roles
+		WHERE id = %s;`, QuoteString(id))
+}
+
+func ReadRoleDatasource() string {
+	return "SELECT id, name FROM mz_roles"
+}
