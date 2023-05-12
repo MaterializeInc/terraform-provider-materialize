166e0fab27f6b07ca725ef535ca7e8b0db7fd0cd
Dennis Hume <dennis@materialize.com>
2023-05-12 09:14:33 -0500

    Update Indexes Query

--- pkg/materialize/index.go
+++ pkg/materialize/index.go
@@ -109,104 +109,41 @@ func (b *IndexBuilder) ReadId() string {
 
 func ReadIndexParams(id string) string {
 	return fmt.Sprintf(`
-		SELECT DISTINCT
+		SELECT
 			mz_indexes.name,
-			COALESCE(sources.o_name, views.o_name, mviews.o_name),
-			COALESCE(sources.s_name, views.s_name, mviews.s_name),
-			COALESCE(sources.d_name, views.d_name, mviews.d_name)
+			mz_objects.name,
+			mz_schemas.name,
+			mz_databases.name
 		FROM mz_indexes
-		LEFT JOIN (
-			SELECT
-				mz_sources.id,
-				mz_sources.name AS o_name,
-				mz_schemas.name AS s_name,
-				mz_databases.name AS d_name
-			FROM mz_sources
-			JOIN mz_schemas
-				ON mz_sources.schema_id = mz_schemas.id
-			JOIN mz_databases
-				ON mz_schemas.database_id = mz_databases.id
-		) sources
-			ON mz_indexes.on_id = sources.id
-		LEFT JOIN (
-			SELECT
-				mz_views.id,
-				mz_views.name AS o_name,
-				mz_schemas.name AS s_name,
-				mz_databases.name AS d_name
-			FROM mz_views
-			JOIN mz_schemas
-				ON mz_views.schema_id = mz_schemas.id
-			JOIN mz_databases
-				ON mz_schemas.database_id = mz_databases.id
-		) views
-			ON mz_indexes.on_id = sources.id
-		LEFT JOIN (
-			SELECT
-				mz_materialized_views.id,
-				mz_materialized_views.name AS o_name,
-				mz_schemas.name AS s_name,
-				mz_databases.name AS d_name
-			FROM mz_materialized_views
-			JOIN mz_schemas
-				ON mz_materialized_views.schema_id = mz_schemas.id
-			JOIN mz_databases
-				ON mz_schemas.database_id = mz_databases.id
-		) mviews
-			ON mz_indexes.on_id = sources.id
-		WHERE mz_indexes.id = %s;`, QuoteString(id))
+		JOIN mz_objects
+			ON mz_indexes.on_id = mz_objects.id
+		LEFT JOIN mz_schemas
+			ON mz_objects.schema_id = mz_schemas.id
+		LEFT JOIN mz_databases
+			ON mz_schemas.database_id = mz_databases.id
+		WHERE mz_objects.type IN ('source', 'view', 'materialized-view')
+		AND mz_indexes.id = %s;
+	`, QuoteString(id))
 }
 
 func ReadIndexDatasource(databaseName, schemaName string) string {
 	q := strings.Builder{}
 	q.WriteString(`
-		SELECT DISTINCT
+		SELECT
 			mz_indexes.id,
 			mz_indexes.name,
-			COALESCE(sources.o_name, views.o_name, mviews.o_name),
-			COALESCE(sources.s_name, views.s_name, mviews.s_name),
-			COALESCE(sources.d_name, views.d_name, mviews.d_name)
+			mz_objects.name,
+			mz_schemas.name,
+			mz_databases.name
 		FROM mz_indexes
-		LEFT JOIN (
-			SELECT
-				mz_sources.id,
-				mz_sources.name AS o_name,
-				mz_schemas.name AS s_name,
-				mz_databases.name AS d_name
-			FROM mz_sources
-			JOIN mz_schemas
-				ON mz_sources.schema_id = mz_schemas.id
-			JOIN mz_databases
-				ON mz_schemas.database_id = mz_databases.id
-		) sources
-			ON mz_indexes.on_id = sources.id
-		LEFT JOIN (
-			SELECT
-				mz_views.id,
-				mz_views.name AS o_name,
-				mz_schemas.name AS s_name,
-				mz_databases.name AS d_name
-			FROM mz_views
-			JOIN mz_schemas
-				ON mz_views.schema_id = mz_schemas.id
-			JOIN mz_databases
-				ON mz_schemas.database_id = mz_databases.id
-		) views
-			ON mz_indexes.on_id = sources.id
-		LEFT JOIN (
-			SELECT
-				mz_materialized_views.id,
-				mz_materialized_views.name AS o_name,
-				mz_schemas.name AS s_name,
-				mz_databases.name AS d_name
-			FROM mz_materialized_views
-			JOIN mz_schemas
-				ON mz_materialized_views.schema_id = mz_schemas.id
-			JOIN mz_databases
-				ON mz_schemas.database_id = mz_databases.id
-		) mviews
-			ON mz_indexes.on_id = sources.id
-		WHERE COALESCE(sources.o_name, views.o_name, mviews.o_name) IS NOT NULL`)
+		JOIN mz_objects
+			ON mz_indexes.on_id = mz_objects.id
+		LEFT JOIN mz_schemas
+			ON mz_objects.schema_id = mz_schemas.id
+		LEFT JOIN mz_databases
+			ON mz_schemas.database_id = mz_databases.id
+		WHERE mz_objects.type IN ('source', 'view', 'materialized-view')
+	`)
 
 	if databaseName != "" {
 		q.WriteString(fmt.Sprintf(`
