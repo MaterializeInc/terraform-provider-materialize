5f30a9bc2389136b8a41e4a6a4bd3386dc32246c
Dennis Hume <dennis@materialize.com>
2023-05-18 09:14:07 -0500

    Terraform fmt

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -1,8 +1,8 @@
 # Configuration-based authentication
 provider "materialize" {
-  host     = var.materialize_hostname  # optionally use MZ_HOST env var
-  username = var.materialize_username  # optionally use MZ_USER env var
-  password = var.materialize_password  # optionally use MZ_PW env var
-  port     = var.materialize_port      # optionally use MZ_PORT env var
-  database = var.materialize_database  # optionally use MZ_DATABASE env var
+  host     = var.materialize_hostname # optionally use MZ_HOST env var
+  username = var.materialize_username # optionally use MZ_USER env var
+  password = var.materialize_password # optionally use MZ_PW env var
+  port     = var.materialize_port     # optionally use MZ_PORT env var
+  database = var.materialize_database # optionally use MZ_DATABASE env var
 }
