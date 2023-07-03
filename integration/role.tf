213f179eb582daa7afb6f1873ba3f7466f8a65b5
Dennis Hume <dennis@materialize.com>
2023-07-03 08:49:15 -0500

    Terraform fmt

--- integration/role.tf
+++ integration/role.tf
@@ -1,9 +1,9 @@
 resource "materialize_role" "role_1" {
-  name           = "role_1"
+  name = "role_1"
 }
 
 resource "materialize_role" "role_2" {
-  name           = "role_2"
+  name = "role_2"
 }
 
 output "qualified_role" {
