aa613adfe18f2b7af8322bee578cd7fe9b53d853
Dennis Hume <dennis@materialize.com>
2023-08-31 15:37:49 -0500

    Read Fix

--- pkg/resources/resource_grant_default_privilege.go
+++ pkg/resources/resource_grant_default_privilege.go
@@ -79,8 +79,14 @@ func grantDefaultPrivilegeRead(ctx context.Context, d *schema.ResourceData, meta
 
 	d.SetId(i)
 
-	if err := d.Set("target_role_name", privileges[0].TargetRole.String); err != nil {
-		return diag.FromErr(err)
+	if privileges[0].TargetRole.String == "p" {
+		if err := d.Set("target_role_name", "PUBLIC"); err != nil {
+			return diag.FromErr(err)
+		}
+	} else {
+		if err := d.Set("target_role_name", privileges[0].TargetRole.String); err != nil {
+			return diag.FromErr(err)
+		}
 	}
 
 	return nil
