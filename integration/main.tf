f0e3384adecafd83129b3440bb0045dec6d3371d
Bobby Iliev <bobby@bobbyiliev.com>
2023-10-30 18:42:34 +0200

    178 Provider configuration params unification (#339)

--- integration/main.tf
+++ integration/main.tf
@@ -8,9 +8,9 @@ terraform {
 
 provider "materialize" {
   host     = "materialized"
-  username = "mz_system"
+  user     = "mz_system"
   password = "password"
   port     = 6877
   database = "materialize"
-  testing  = true
+  sslmode  = false
 }
