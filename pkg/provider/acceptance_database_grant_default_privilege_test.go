b991fd530acad497f936f1d2fdc64de2dc8bef0b
Dennis Hume <dennis@materialize.com>
2023-11-17 11:29:36 -0600

    Remove default depends_on

--- pkg/provider/acceptance_database_grant_default_privilege_test.go
+++ pkg/provider/acceptance_database_grant_default_privilege_test.go
@@ -72,8 +72,6 @@ resource "materialize_database_grant_default_privilege" "test" {
 	grantee_name     = materialize_role.test_grantee.name
 	privilege        = "%[3]s"
 	target_role_name = materialize_role.test_target.name
-
-	depends_on = [materialize_role.test_grantee, materialize_role.test_target]
 }
 `, granteeName, targetName, privilege)
 }
