665853a93575d2c5c39b5956dc2e2ed790dedd4c
Bobby Iliev <bobby@bobbyiliev.com>
2023-12-06 13:36:46 +0200

    Add id migrations test

--- examples/resources/materialize_database/import.sh
+++ examples/resources/materialize_database/import.sh
@@ -1,4 +1,5 @@
 # Databases can be imported using the database id:
-terraform import materialize_database.example_database <database_id>
+terraform import materialize_database.example_database <region>:<database_id>
 
 # Database id and information be found in the `mz_catalog.mz_databases` table
+# The region is the region where the database is located (e.g. aws/us-east-1)
