ba09f91a4c0d0db1431703a295e28ec6b970ce95
Bobby Iliev <bobby@bobbyiliev.com>
2023-12-07 16:41:09 +0200

    Update examples/resources/materialize_type/import.sh
    Co-authored-by: Dennis Hume <dehume@users.noreply.github.com>

--- examples/resources/materialize_type/import.sh
+++ examples/resources/materialize_type/import.sh
@@ -3,4 +3,3 @@ terraform import materialize_type.example_type <region>:<type_id>
 
 # Type id and information be found in the `mz_catalog.mz_types` table
 # The region is the region where the database is located (e.g. aws/us-east-1)
-```
