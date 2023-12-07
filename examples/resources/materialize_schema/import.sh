828b2d17dd13ffe683d605d32cf41c7864acd63e
Bobby Iliev <bobby@bobbyiliev.com>
2023-12-07 16:40:53 +0200

    Update examples/resources/materialize_schema/import.sh
    Co-authored-by: Dennis Hume <dehume@users.noreply.github.com>

--- examples/resources/materialize_schema/import.sh
+++ examples/resources/materialize_schema/import.sh
@@ -1,5 +1,5 @@
 # Schemas can be imported using the schema id:
-terraform import materialize_schema.example_schema <role>:<schema_id>
+terraform import materialize_schema.example_schema <region>:<schema_id>
 
 # Schema id and information be found in the `mz_catalog.mz_schemas` table
 # The role is the role where the database is located (e.g. aws/us-east-1)
