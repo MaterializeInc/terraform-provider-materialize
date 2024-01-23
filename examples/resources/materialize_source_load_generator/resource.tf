9da57129a416e8d121d20f263096d6a2f71f2991
Bobby Iliev <bobby@bobbyiliev.com>
2024-01-23 20:42:45 +0200

    Remove SIZE from documentation examples

--- examples/resources/materialize_source_load_generator/resource.tf
+++ examples/resources/materialize_source_load_generator/resource.tf
@@ -1,7 +1,7 @@
 resource "materialize_source_load_generator" "example_source_load_generator" {
-  name        = "source_load_generator"
-  schema_name = "schema"
-  size        = "3xsmall"
+  name         = "source_load_generator"
+  schema_name  = "schema"
+  cluster_name = "quickstart"
 
   load_generator_type = "COUNTER"
 
