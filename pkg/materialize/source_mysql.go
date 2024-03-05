0e5e557039b841dc68bc62722fc626db0a0a5d68
Bobby Iliev <bobby@bobbyiliev.com>
2024-03-05 11:40:26 +0200

    Add MySQL source

---
+++ pkg/materialize/source_mysql.go
@@ -0,0 +1,55 @@
+package materialize
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/jmoiron/sqlx"
+)
+
+type SourceMySQLBuilder struct {
+	Source
+	mysqlConnection IdentifierSchemaStruct
+	tables          []TableStruct
+}
+
+func NewSourceMySQLBuilder(conn *sqlx.DB, obj MaterializeObject) *SourceMySQLBuilder {
+	b := Builder{conn, BaseSource}
+	return &SourceMySQLBuilder{
+		Source: Source{b, obj.Name, obj.SchemaName, obj.DatabaseName},
+	}
+}
+
+func (b *SourceMySQLBuilder) MySQLConnection(mysqlConn IdentifierSchemaStruct) *SourceMySQLBuilder {
+	b.mysqlConnection = mysqlConn
+	return b
+}
+
+func (b *SourceMySQLBuilder) Tables(tables []TableStruct) *SourceMySQLBuilder {
+	b.tables = tables
+	return b
+}
+
+func (b *SourceMySQLBuilder) Create() error {
+	q := strings.Builder{}
+	q.WriteString(fmt.Sprintf(`CREATE SOURCE %s FROM MYSQL CONNECTION %s`, b.QualifiedName(), b.mysqlConnection.QualifiedName()))
+
+	if len(b.tables) > 0 {
+		q.WriteString(` FOR TABLES (`)
+		for i, table := range b.tables {
+			if table.Alias == "" {
+				table.Alias = table.Name
+			}
+			q.WriteString(fmt.Sprintf(`%s AS %s`, table.Name, table.Alias))
+			if i < len(b.tables)-1 {
+				q.WriteString(`, `)
+			}
+		}
+		q.WriteString(`)`)
+	} else {
+		q.WriteString(` FOR ALL TABLES`)
+	}
+
+	q.WriteString(`;`)
+	return b.ddl.exec(q.String())
+}
---
+++ pkg/materialize/source_mysql_test.go
@@ -0,0 +1,82 @@
+package materialize
+
+import (
+	"testing"
+
+	sqlmock "github.com/DATA-DOG/go-sqlmock"
+	"github.com/MaterializeInc/terraform-provider-materialize/pkg/testhelpers"
+	"github.com/jmoiron/sqlx"
+)
+
+var sourceMySQL = MaterializeObject{Name: "source", SchemaName: "schema", DatabaseName: "database"}
+var tableInputMySQL = []TableStruct{
+	{Name: "table_1"},
+	{Name: "table_2", Alias: "table_alias"},
+}
+
+func TestSourceMySQLAllTablesCreate(t *testing.T) {
+	testhelpers.WithMockDb(t, func(db *sqlx.DB, mock sqlmock.Sqlmock) {
+		mock.ExpectExec(
+			`CREATE SOURCE "database"."schema"."source" FROM MYSQL CONNECTION "database"."schema"."mysql_connection" FOR ALL TABLES;`,
+		).WillReturnResult(sqlmock.NewResult(1, 1))
+
+		b := NewSourceMySQLBuilder(db, sourceMySQL)
+		b.MySQLConnection(IdentifierSchemaStruct{Name: "mysql_connection", SchemaName: "schema", DatabaseName: "database"})
+
+		if err := b.Create(); err != nil {
+			t.Fatal(err)
+		}
+	})
+}
+
+func TestSourceMySQLSpecificTablesCreate(t *testing.T) {
+	testhelpers.WithMockDb(t, func(db *sqlx.DB, mock sqlmock.Sqlmock) {
+		mock.ExpectExec(
+			`CREATE SOURCE "database"."schema"."source" FROM MYSQL CONNECTION "database"."schema"."mysql_connection" FOR TABLES \(schema1.table_1 AS s1_table_1, schema2.table_2 AS table_alias\);`,
+		).WillReturnResult(sqlmock.NewResult(1, 1))
+
+		b := NewSourceMySQLBuilder(db, sourceMySQL)
+		b.MySQLConnection(IdentifierSchemaStruct{Name: "mysql_connection", SchemaName: "schema", DatabaseName: "database"})
+		b.Tables([]TableStruct{
+			{
+				Name:  "schema1.table_1",
+				Alias: "s1_table_1",
+			},
+			{
+				Name:  "schema2.table_2",
+				Alias: "table_alias",
+			},
+		})
+
+		if err := b.Create(); err != nil {
+			t.Fatal(err)
+		}
+	})
+}
+
+// TODO: Add sub source tests if needed
+// func TestSourceMySQLAddSubsource(t *testing.T) {
+// 	testhelpers.WithMockDb(t, func(db *sqlx.DB, mock sqlmock.Sqlmock) {
+// 		mock.ExpectExec(
+// 			`ALTER SOURCE "database"."schema"."source" ADD SUBSOURCE "table_1", "table_2" AS "table_alias";`,
+// 		).WillReturnResult(sqlmock.NewResult(1, 1))
+
+// 		b := NewSource(db, sourceMySQL)
+// 		if err := b.AddSubsource(tableInputMySQL, []string{}); err != nil {
+// 			t.Fatal(err)
+// 		}
+// 	})
+// }
+
+// func TestSourceMySQLDropSubsource(t *testing.T) {
+// 	testhelpers.WithMockDb(t, func(db *sqlx.DB, mock sqlmock.Sqlmock) {
+// 		mock.ExpectExec(
+// 			`ALTER SOURCE "database"."schema"."source" DROP SUBSOURCE "table_1", "table_alias";`,
+// 		).WillReturnResult(sqlmock.NewResult(1, 1))
+
+// 		b := NewSourceMySQLBuilder(db, sourceMySQL)
+// 		if err := b.DropSubsource(tableInputMySQL); err != nil {
+// 			t.Fatal(err)
+// 		}
+// 	})
+// }
