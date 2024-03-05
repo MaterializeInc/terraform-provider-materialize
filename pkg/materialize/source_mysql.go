a3f59ea80f5736310819e3cebe97c3168cbaff99
Bobby Iliev <bobby@bobbyiliev.com>
2024-03-05 12:15:35 +0200

    Add MySQL source resource

--- pkg/materialize/source_mysql.go
+++ pkg/materialize/source_mysql.go
@@ -9,6 +9,8 @@ import (
 
 type SourceMySQLBuilder struct {
 	Source
+	clusterName     string
+	size            string
 	mysqlConnection IdentifierSchemaStruct
 	tables          []TableStruct
 }
@@ -18,6 +20,16 @@ func NewSourceMySQLBuilder(conn *sqlx.DB, obj MaterializeObject) *SourceMySQLBui
 	return &SourceMySQLBuilder{
 		Source: Source{b, obj.Name, obj.SchemaName, obj.DatabaseName},
 	}
+}
+
+func (b *SourceMySQLBuilder) ClusterName(c string) *SourceMySQLBuilder {
+	b.clusterName = c
+	return b
+}
+
+func (b *SourceMySQLBuilder) Size(s string) *SourceMySQLBuilder {
+	b.size = s
+	return b
 }
 
 func (b *SourceMySQLBuilder) MySQLConnection(mysqlConn IdentifierSchemaStruct) *SourceMySQLBuilder {
@@ -32,7 +44,14 @@ func (b *SourceMySQLBuilder) Tables(tables []TableStruct) *SourceMySQLBuilder {
 
 func (b *SourceMySQLBuilder) Create() error {
 	q := strings.Builder{}
-	q.WriteString(fmt.Sprintf(`CREATE SOURCE %s FROM MYSQL CONNECTION %s`, b.QualifiedName(), b.mysqlConnection.QualifiedName()))
+
+	q.WriteString(fmt.Sprintf(`CREATE SOURCE %s`, b.QualifiedName()))
+
+	if b.clusterName != "" {
+		q.WriteString(fmt.Sprintf(` IN CLUSTER %s`, QuoteIdentifier(b.clusterName)))
+	}
+
+	q.WriteString(fmt.Sprintf(` FROM MYSQL CONNECTION %s`, b.mysqlConnection.QualifiedName()))
 
 	if len(b.tables) > 0 {
 		q.WriteString(` FOR TABLES (`)
