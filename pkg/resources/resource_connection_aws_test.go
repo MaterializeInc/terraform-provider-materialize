2d89cb0a9d22d39206ec79363d6fb877cbfc73d6
Bobby Iliev <bobby@bobbyiliev.com>
2024-04-01 17:54:47 +0300

    Fix failing unit tests

--- pkg/resources/resource_connection_aws_test.go
+++ pkg/resources/resource_connection_aws_test.go
@@ -13,10 +13,16 @@ import (
 )
 
 var inAws = map[string]interface{}{
-	"name":          "conn",
-	"schema_name":   "schema",
-	"database_name": "database",
-	"access_key_id": []interface{}{map[string]interface{}{"text": "foo"}},
+	"name":                     "conn",
+	"endpoint":                 "http://localhost:4566",
+	"aws_region":               "us-east-1",
+	"schema_name":              "schema",
+	"database_name":            "database",
+	"access_key_id":            []interface{}{map[string]interface{}{"text": "foo"}},
+	"secret_access_key":        []interface{}{map[string]interface{}{"name": "conn_secret"}},
+	"session_token":            []interface{}{map[string]interface{}{"secret": []interface{}{map[string]interface{}{"name": "conn_session"}}}},
+	"assume_role_arn":          "arn:aws:iam::123456789012:role/role",
+	"assume_role_session_name": "session",
 }
 
 func TestResourceConnectionAwsCreate(t *testing.T) {
@@ -27,8 +33,7 @@ func TestResourceConnectionAwsCreate(t *testing.T) {
 	testhelpers.WithMockProviderMeta(t, func(db *utils.ProviderMeta, mock sqlmock.Sqlmock) {
 		// Create
 		mock.ExpectExec(
-			`CREATE CONNECTION "database"."schema"."conn"
-			TO AWS \( ACCESS KEY ID = 'foo'\);`,
+			`CREATE CONNECTION "database"."schema"."conn" TO AWS \( ENDPOINT = 'http://localhost:4566', REGION = 'us-east-1', ACCESS KEY ID = 'foo', SECRET ACCESS KEY = SECRET "materialize"."public"."conn_secret", SESSION TOKEN = SECRET "materialize"."public"."conn_session", ASSUME ROLE ARN = 'arn:aws:iam::123456789012:role/role', ASSUME ROLE SESSION NAME = 'session'\);`,
 		).WillReturnResult(sqlmock.NewResult(1, 1))
 
 		// Query Id
@@ -82,6 +87,13 @@ func TestResourceConnectionAwsUpdate(t *testing.T) {
 
 	testhelpers.WithMockProviderMeta(t, func(db *utils.ProviderMeta, mock sqlmock.Sqlmock) {
 		mock.ExpectExec(`ALTER CONNECTION "database"."schema"."" RENAME TO "conn";`).WillReturnResult(sqlmock.NewResult(1, 1))
+
+		mock.ExpectExec(`ALTER CONNECTION "database"."schema"."old_conn" SET \(ENDPOINT = 'http://localhost:4566'\);`).WillReturnResult(sqlmock.NewResult(1, 1))
+		mock.ExpectExec(`ALTER CONNECTION "database"."schema"."old_conn" SET \(REGION = 'us-east-1'\);`).WillReturnResult(sqlmock.NewResult(1, 1))
+		mock.ExpectExec(`ALTER CONNECTION "database"."schema"."old_conn" SET \(ACCESS KEY ID = 'foo'\);`).WillReturnResult(sqlmock.NewResult(1, 1))
+		mock.ExpectExec(`ALTER CONNECTION "database"."schema"."old_conn" SET \(SECRET ACCESS KEY = SECRET "materialize"."public"."conn_secret"\);`).WillReturnResult(sqlmock.NewResult(1, 1))
+		mock.ExpectExec(`ALTER CONNECTION "database"."schema"."old_conn" SET \(SESSION TOKEN = SECRET "materialize"."public"."conn_session"\);`).WillReturnResult(sqlmock.NewResult(1, 1))
+		mock.ExpectExec(`ALTER CONNECTION "database"."schema"."old_conn" SET \(ASSUME ROLE ARN = 'arn:aws:iam::123456789012:role/role'\), SET\(ASSUME ROLE SESSION NAME = 'session'\);`).WillReturnResult(sqlmock.NewResult(1, 1))
 
 		// Query Params
 		pp := `WHERE mz_connections.id = 'u1'`
