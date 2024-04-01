0ec5918deb287d71e852650617e9f98b7b5c0640
Bobby Iliev <bobby@bobbyiliev.com>
2024-04-01 15:55:41 +0300

    Allow alter connection params for AWS connections

--- pkg/resources/resource_connection_aws.go
+++ pkg/resources/resource_connection_aws.go
@@ -22,33 +22,33 @@ var connectionAwsSchema = map[string]*schema.Schema{
 		Description: "Override the default AWS endpoint URL.",
 		Type:        schema.TypeString,
 		Optional:    true,
-		ForceNew:    true,
+		ForceNew:    false,
 	},
 	"aws_region": {
 		Description: "The AWS region to connect to.",
 		Type:        schema.TypeString,
 		Optional:    true,
-		ForceNew:    true,
+		ForceNew:    false,
 	},
-	"access_key_id": ValueSecretSchema("access_key_id", "The access key ID to connect with.", false, true),
+	"access_key_id": ValueSecretSchema("access_key_id", "The access key ID to connect with.", false, false),
 	"secret_access_key": IdentifierSchema(IdentifierSchemaParams{
 		Elem:        "secret_access_key",
 		Description: "The secret access key corresponding to the specified access key ID.",
 		Required:    false,
-		ForceNew:    true,
+		ForceNew:    false,
 	}),
-	"session_token": ValueSecretSchema("session_token", "The session token corresponding to the specified access key ID.", false, true),
+	"session_token": ValueSecretSchema("session_token", "The session token corresponding to the specified access key ID.", false, false),
 	"assume_role_arn": {
 		Description: "The Amazon Resource Name (ARN) of the IAM role to assume.",
 		Type:        schema.TypeString,
 		Optional:    true,
-		ForceNew:    true,
+		ForceNew:    false,
 	},
 	"assume_role_session_name": {
 		Description:  "The session name to use when assuming the role.",
 		Type:         schema.TypeString,
 		Optional:     true,
-		ForceNew:     true,
+		ForceNew:     false,
 		RequiredWith: []string{"assume_role_arn"},
 	},
 	"validate":       ValidateConnectionSchema(),
@@ -244,6 +244,7 @@ func connectionAwsUpdate(ctx context.Context, d *schema.ResourceData, meta inter
 	connectionName := d.Get("name").(string)
 	schemaName := d.Get("schema_name").(string)
 	databaseName := d.Get("database_name").(string)
+	validate := d.Get("validate").(bool)
 
 	metaDb, _, err := utils.GetDBClientFromMeta(meta, d)
 	if err != nil {
@@ -257,6 +258,130 @@ func connectionAwsUpdate(ctx context.Context, d *schema.ResourceData, meta inter
 		b := materialize.NewConnectionAwsBuilder(metaDb, o)
 		if err := b.Rename(newName.(string)); err != nil {
 			return diag.FromErr(err)
+		}
+	}
+
+	if d.HasChange("endpoint") {
+		oldEndpoint, newEndpoint := d.GetChange("endpoint")
+		b := materialize.NewConnection(metaDb, o)
+		if newEndpoint == nil || newEndpoint == "" {
+			if err := b.AlterDrop([]string{"ENDPOINT"}, validate); err != nil {
+				d.Set("endpoint", oldEndpoint)
+				return diag.FromErr(err)
+			}
+		} else {
+			options := map[string]interface{}{
+				"ENDPOINT": newEndpoint.(string),
+			}
+			if err := b.Alter(options, false, validate); err != nil {
+				d.Set("host", oldEndpoint)
+				return diag.FromErr(err)
+			}
+		}
+	}
+
+	if d.HasChange("aws_region") {
+		oldRegion, newRegion := d.GetChange("aws_region")
+		b := materialize.NewConnection(metaDb, o)
+		if newRegion == nil || newRegion == "" {
+			if err := b.AlterDrop([]string{"REGION"}, validate); err != nil {
+				d.Set("aws_region", oldRegion)
+				return diag.FromErr(err)
+			}
+		} else {
+			options := map[string]interface{}{
+				"REGION": newRegion.(string),
+			}
+			if err := b.Alter(options, false, validate); err != nil {
+				d.Set("aws_region", oldRegion)
+				return diag.FromErr(err)
+			}
+		}
+	}
+
+	if d.HasChange("access_key_id") {
+		oldAccessKeyId, newAccessKeyId := d.GetChange("access_key_id")
+		b := materialize.NewConnectionAwsBuilder(metaDb, o)
+		if newAccessKeyId == nil || newAccessKeyId == "" || len(newAccessKeyId.([]interface{})) == 0 {
+			// TODO: Can't drop access key id without secret session token
+			if err := b.AlterDrop([]string{"ACCESS KEY ID"}, validate); err != nil {
+				d.Set("access_key_id", oldAccessKeyId)
+				return diag.FromErr(err)
+			}
+		} else {
+			options := map[string]interface{}{
+				"ACCESS KEY ID": materialize.GetValueSecretStruct(newAccessKeyId),
+			}
+			if err := b.Alter(options, false, validate); err != nil {
+				d.Set("access_key_id", oldAccessKeyId)
+				return diag.FromErr(err)
+			}
+		}
+	}
+
+	if d.HasChange("secret_access_key") {
+		oldSecretAccessKey, newSecretAccessKey := d.GetChange("secret_access_key")
+		b := materialize.NewConnectionAwsBuilder(metaDb, o)
+		if newSecretAccessKey == nil || newSecretAccessKey == "" || len(newSecretAccessKey.([]interface{})) == 0 {
+			if err := b.AlterDrop([]string{"SECRET ACCESS KEY"}, validate); err != nil {
+				d.Set("secret_access_key", oldSecretAccessKey)
+				return diag.FromErr(err)
+			}
+		} else {
+			options := map[string]interface{}{
+				"SECRET ACCESS KEY": materialize.GetIdentifierSchemaStruct(newSecretAccessKey),
+			}
+			if err := b.Alter(options, true, validate); err != nil {
+				d.Set("secret_access_key", oldSecretAccessKey)
+				return diag.FromErr(err)
+			}
+		}
+	}
+
+	if d.HasChange("session_token") {
+		oldSessionToken, newSessionToken := d.GetChange("session_token")
+		b := materialize.NewConnectionAwsBuilder(metaDb, o)
+		if newSessionToken == nil || newSessionToken == "" || len(newSessionToken.([]interface{})) == 0 {
+			if err := b.AlterDrop([]string{"SESSION TOKEN"}, validate); err != nil {
+				d.Set("session_token", oldSessionToken)
+				return diag.FromErr(err)
+			}
+		} else {
+			options := map[string]interface{}{
+				"SESSION TOKEN": materialize.GetValueSecretStruct(newSessionToken),
+			}
+			if err := b.Alter(options, false, validate); err != nil {
+				d.Set("session_token", oldSessionToken)
+				return diag.FromErr(err)
+			}
+		}
+	}
+
+	if d.HasChange("assume_role_arn") || d.HasChange("assume_role_session_name") {
+		oldAssumeRoleArn, newAssumeRoleArn := d.GetChange("assume_role_arn")
+		oldAssumeRoleSessionName, newAssumeRoleSessionName := d.GetChange("assume_role_session_name")
+		b := materialize.NewConnectionAwsBuilder(metaDb, o)
+		options := make(map[string]interface{})
+
+		if newAssumeRoleArn != nil && newAssumeRoleArn != "" {
+			options["ASSUME ROLE ARN"] = newAssumeRoleArn.(string)
+		} else if d.HasChange("assume_role_arn") {
+			options["ASSUME ROLE ARN"] = ""
+		}
+
+		if newAssumeRoleSessionName != nil && newAssumeRoleSessionName != "" {
+			options["ASSUME ROLE SESSION NAME"] = newAssumeRoleSessionName.(string)
+		} else if d.HasChange("assume_role_session_name") {
+			options["ASSUME ROLE SESSION NAME"] = ""
+		}
+
+		// Perform the alteration only if there are options to update
+		if len(options) > 0 {
+			if err := b.Alter(options, false, validate); err != nil {
+				d.Set("assume_role_arn", oldAssumeRoleArn)
+				d.Set("assume_role_session_name", oldAssumeRoleSessionName)
+				return diag.FromErr(err)
+			}
 		}
 	}
 
