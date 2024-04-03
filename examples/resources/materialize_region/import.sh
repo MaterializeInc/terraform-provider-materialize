1c290104828c68d88a1a4de8fceab662f2c5282d
Bobby Iliev <bobby@bobbyiliev.com>
2024-04-03 21:35:08 +0300

    Add docs

---
+++ examples/resources/materialize_region/import.sh
@@ -0,0 +1,2 @@
+# Regions can be imported using the `terraform import` command
+terraform import materialize_region.example_region aws/us-east-1
---
+++ examples/resources/materialize_region/resource.tf
@@ -0,0 +1,3 @@
+resource "materialize_region" "example" {
+  region_id = "aws/eu-west-2"
+}
