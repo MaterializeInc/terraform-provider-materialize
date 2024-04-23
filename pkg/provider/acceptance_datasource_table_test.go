d9cf7f051e4df1ac83b8185260eef5428d9c9c7c
Bobby Iliev <bobby@bobbyiliev.com>
2024-04-23 16:16:48 +0300

    Fix failing data source acceptance tests

--- pkg/provider/acceptance_datasource_table_test.go
+++ pkg/provider/acceptance_datasource_table_test.go
@@ -24,7 +24,7 @@ func TestAccDatasourceTable_basic(t *testing.T) {
 					resource.TestCheckResourceAttr("data.materialize_table.test_database", "tables.#", "3"),
 					resource.TestCheckResourceAttr("data.materialize_table.test_database_schema", "database_name", nameSpace),
 					resource.TestCheckResourceAttr("data.materialize_table.test_database_schema", "schema_name", nameSpace),
-					resource.TestCheckResourceAttr("data.materialize_table.test_database_schema", "tables.#", "2"),
+					resource.TestCheckResourceAttr("data.materialize_table.test_database_schema", "tables.#", "3"),
 					resource.TestCheckResourceAttr("data.materialize_table.test_database_2", "database_name", nameSpace+"_2"),
 					resource.TestCheckNoResourceAttr("data.materialize_table.test_database_2", "schema_name"),
 					resource.TestCheckResourceAttr("data.materialize_table.test_database_2", "tables.#", "2"),
@@ -49,25 +49,21 @@ func testAccDatasourceTable(nameSpace string) string {
 		name    = "%[1]s_2"
 	}
 
-	resource "materialize_schema" "public_schema" {
-		name          = "public"
-		database_name = materialize_database.test.name
-	}
-
-	resource "materialize_schema" "public_schema2" {
-		name          = "public"
-		database_name = materialize_database.test_2.name
-	}
-
 	resource "materialize_schema" "test" {
 		name          = "%[1]s"
 		database_name = materialize_database.test.name
 	}
 
+	resource "materialize_schema" "test_2" {
+		name          = "%[1]s_2"
+		database_name = materialize_database.test_2.name
+	}
+
+
 	resource "materialize_table" "a" {
 		name          = "%[1]s_a"
 		database_name = materialize_database.test.name
-		schema_name   = materialize_schema.public_schema.name
+		schema_name   = materialize_schema.test.name
 		comment       = "some comment"
 
 		column {
@@ -80,7 +76,7 @@ func testAccDatasourceTable(nameSpace string) string {
 	resource "materialize_table" "b" {
 		name          = "%[1]s_b"
 		database_name = materialize_database.test.name
-		schema_name   = materialize_schema.public_schema.name
+		schema_name   = materialize_schema.test.name
 		column {
 			name = "column_1"
 			type = "text"
@@ -90,7 +86,7 @@ func testAccDatasourceTable(nameSpace string) string {
 	resource "materialize_table" "c" {
 		name          = "%[1]s_c"
 		database_name = materialize_database.test.name
-		schema_name   = materialize_schema.public_schema.name
+		schema_name   = materialize_schema.test.name
 		comment       = "some comment"
 
 		column {
@@ -103,7 +99,7 @@ func testAccDatasourceTable(nameSpace string) string {
 	resource "materialize_table" "d" {
 		name          = "%[1]s_d"
 		database_name = materialize_database.test_2.name
-		schema_name   = materialize_schema.public_schema2.name
+		schema_name   = materialize_schema.test_2.name
 		column {
 			name = "column_1"
 			type = "text"
@@ -113,7 +109,7 @@ func testAccDatasourceTable(nameSpace string) string {
 	resource "materialize_table" "e" {
 		name          = "%[1]s_e"
 		database_name = materialize_database.test_2.name
-		schema_name   = materialize_schema.public_schema2.name
+		schema_name   = materialize_schema.test_2.name
 		column {
 			name = "column_1"
 			type = "text"
