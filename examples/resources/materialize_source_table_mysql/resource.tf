a21776732e2690b3105fc06a7cb8d26ae1526fd2
Bobby Iliev <bobby@bobbyiliev.com>
2024-09-13 15:27:58 +0300

    Format examples

--- examples/resources/materialize_source_table_mysql/resource.tf
+++ examples/resources/materialize_source_table_mysql/resource.tf
@@ -1,7 +1,7 @@
 resource "materialize_source_table_mysql" "mysql_table_from_source" {
-  name           = "mysql_table_from_source"
-  schema_name    = "public"
-  database_name  = "materialize"
+  name          = "mysql_table_from_source"
+  schema_name   = "public"
+  database_name = "materialize"
 
   source {
     name          = materialize_source_mysql.example.name
@@ -9,8 +9,8 @@ resource "materialize_source_table_mysql" "mysql_table_from_source" {
     database_name = materialize_source_mysql.example.database_name
   }
 
-  upstream_name         = "mysql_table_name" # The name of the table in the MySQL database
-  upstream_schema_name  = "mysql_db_name"    # The name of the database in the MySQL database
+  upstream_name        = "mysql_table_name" # The name of the table in the MySQL database
+  upstream_schema_name = "mysql_db_name"    # The name of the database in the MySQL database
 
   text_columns = [
     "updated_at"
--- examples/resources/materialize_source_table_postgres/resource.tf
+++ examples/resources/materialize_source_table_postgres/resource.tf
@@ -1,7 +1,7 @@
 resource "materialize_source_table_postgres" "postgres_table_from_source" {
-  name           = "postgres_table_from_source"
-  schema_name    = "public"
-  database_name  = "materialize"
+  name          = "postgres_table_from_source"
+  schema_name   = "public"
+  database_name = "materialize"
 
   source {
     name          = materialize_source_postgres.example.name
@@ -9,8 +9,8 @@ resource "materialize_source_table_postgres" "postgres_table_from_source" {
     database_name = materialize_source_postgres.example.database_name
   }
 
-  upstream_name         = "postgres_table_name"   # The name of the table in the postgres database
-  upstream_schema_name  = "postgres_schema_name"  # The name of the database in the postgres database
+  upstream_name        = "postgres_table_name"  # The name of the table in the postgres database
+  upstream_schema_name = "postgres_schema_name" # The name of the database in the postgres database
 
   text_columns = [
     "updated_at"
