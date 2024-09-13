bc71db14c1ac9af55b19d7049a12755b9cd76f73
Bobby Iliev <bobby@bobbyiliev.com>
2024-09-13 15:27:58 +0300

    Format examples

--- examples/resources/materialize_source_table_load_generator/resource.tf
+++ examples/resources/materialize_source_table_load_generator/resource.tf
@@ -1,7 +1,7 @@
 resource "materialize_source_table_load_generator" "load_generator_table_from_source" {
-  name           = "load_generator_table_from_source"
-  schema_name    = "public"
-  database_name  = "materialize"
+  name          = "load_generator_table_from_source"
+  schema_name   = "public"
+  database_name = "materialize"
 
   # The load generator source must be of type: `auction_options`, `marketing_options`, and `tpch_options` load generator sources.
   source {
@@ -10,6 +10,6 @@ resource "materialize_source_table_load_generator" "load_generator_table_from_so
     database_name = materialize_source_load_generator.example.database_name
   }
 
-  upstream_name         = "load_generator_table_name" # The name of the table from the load generator
+  upstream_name = "load_generator_table_name" # The name of the table from the load generator
 
 }
