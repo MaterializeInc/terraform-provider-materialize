8e7572c9bfa35faba9125121f064e5e7e40b5bf0
Bobby Iliev <bobby@bobbyiliev.com>
2024-09-16 15:18:38 +0300

    Add Kafka source table resource

--- pkg/resources/resource_source_load_generator.go
+++ pkg/resources/resource_source_load_generator.go
@@ -175,7 +175,7 @@ var sourceLoadgenSchema = map[string]*schema.Schema{
 		ConflictsWith: []string{"counter_options", "auction_options", "marketing_options", "tpch_options"},
 	},
 	"all_tables": {
-		Description:   "Whether to include all tables in the source. Compatible with `auction_options`, `marketing_options`, and `tpch_options`. If not specified, use the `materialize_source_table` resource to specify tables to include.",
+		Description:   "Whether to include all tables in the source. Compatible with `auction_options`, `marketing_options`, and `tpch_options`. If not specified, use the `materialize_source_table_load_generator` resource to specify tables to include.",
 		Type:          schema.TypeBool,
 		Optional:      true,
 		Default:       false,
