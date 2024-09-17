f81f05b85dc4bed87eaed0938a857b387e5a052d
Bobby Iliev <bobby@bobbyiliev.com>
2024-09-17 16:10:15 +0300

    Add import examples for Kafka source tables

--- examples/resources/materialize_source_table_kafka/import.sh
+++ examples/resources/materialize_source_table_kafka/import.sh
@@ -1,5 +1,5 @@
 # Source tables can be imported using the source table id:
-terraform import materialize_source_table_mysql.example_source_table_mysql <region>:<source_table_id>
+terraform import materialize_source_table_kafka.example_source_table_kafka <region>:<source_table_id>
 
 # Source id and information be found in the `mz_catalog.mz_tables` table
 # The region is the region where the database is located (e.g. aws/us-east-1)
--- examples/resources/materialize_source_table_postgres/import.sh
+++ examples/resources/materialize_source_table_postgres/import.sh
@@ -1,5 +1,5 @@
-# Source tables can be imported using the source id:
-terraform import materialize_source_table_postgres.example_source_table_postgres <region>:<source_id>
+# Source tables can be imported using the source table id:
+terraform import materialize_source_table_postgres.example_source_table_postgres <region>:<source_table_id>
 
 # Source id and information be found in the `mz_catalog.mz_tables` table
 # The region is the region where the database is located (e.g. aws/us-east-1)
