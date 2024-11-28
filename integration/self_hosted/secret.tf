b30459a9d78543ee65ff3cc4025f2e1865f4049b
Bobby Iliev <bobby@bobbyiliev.com>
2024-11-28 17:36:37 +0200

    Add integration tests for self hosted

---
+++ integration/self_hosted/secret.tf
@@ -0,0 +1,66 @@
+resource "materialize_secret" "password" {
+  name    = "password"
+  value   = "c2VjcmV0Cg=="
+  comment = "secret comment"
+}
+
+resource "materialize_secret" "postgres_password" {
+  name          = "pg_pass"
+  schema_name   = materialize_schema.schema.name
+  database_name = materialize_database.database.name
+  value         = "c2VjcmV0Cg=="
+}
+
+resource "materialize_secret" "mysql_password" {
+  name          = "mysql__pass"
+  schema_name   = materialize_schema.schema.name
+  database_name = materialize_database.database.name
+  value         = "c2VjcmV0Cg=="
+}
+
+resource "materialize_secret" "mysql_user" {
+  name          = "mysql_user"
+  schema_name   = materialize_schema.schema.name
+  database_name = materialize_database.database.name
+  value         = "repluser"
+}
+
+resource "materialize_secret" "kafka_password" {
+  name          = "kafka_pass"
+  schema_name   = materialize_schema.schema.name
+  database_name = materialize_database.database.name
+  value         = "c2VjcmV0Cg=="
+}
+
+resource "materialize_secret" "aws_password" {
+  name          = "aws_password"
+  schema_name   = materialize_schema.schema.name
+  database_name = materialize_database.database.name
+  value         = "test"
+}
+
+resource "materialize_secret_grant" "secret_grant_usage" {
+  role_name     = materialize_role.role_1.name
+  privilege     = "USAGE"
+  secret_name   = materialize_secret.password.name
+  schema_name   = materialize_secret.password.schema_name
+  database_name = materialize_secret.password.database_name
+}
+
+resource "materialize_secret_grant_default_privilege" "example" {
+  grantee_name     = materialize_role.grantee.name
+  privilege        = "USAGE"
+  target_role_name = materialize_role.target.name
+  schema_name      = materialize_schema.schema.name
+  database_name    = materialize_database.database.name
+}
+
+output "qualified_sql_password" {
+  value = materialize_secret.password.qualified_sql_name
+}
+
+output "qualified_kafka_password" {
+  value = materialize_secret.kafka_password.qualified_sql_name
+}
+
+data "materialize_secret" "all" {}
