ee7807059f06047b8b5c241f3717255c8a87a0ee
Bobby Iliev <bobby@bobbyiliev.com>
2025-09-02 17:40:40 +0300

    Add password and superuser args to user role resource

--- pkg/testhelpers/mock_scans.go
+++ pkg/testhelpers/mock_scans.go
@@ -412,6 +412,7 @@ func MockRoleScan(mock sqlmock.Sqlmock, predicate string) {
 		mz_roles.id,
 		mz_roles.name AS role_name,
 		mz_roles.inherit,
+		mz_roles.superuser,
 		comments.comment AS comment
 	FROM mz_roles
 	LEFT JOIN \(
@@ -422,8 +423,8 @@ func MockRoleScan(mock sqlmock.Sqlmock, predicate string) {
 		ON mz_roles.id = comments.id`
 
 	q := mockQueryBuilder(b, predicate, "")
-	ir := mock.NewRows([]string{"id", "role_name", "inherit"}).
-		AddRow("u1", "joe", true)
+	ir := mock.NewRows([]string{"id", "role_name", "inherit", "superuser", "comment"}).
+		AddRow("u1", "joe", true, false, "")
 	mock.ExpectQuery(q).WillReturnRows(ir)
 }
 
