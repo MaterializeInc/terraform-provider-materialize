62d13ddf1d3ddc2d862fbd576e85a0980974a516
bobbyiliev <bobby@bobbyiliev.com>
2025-12-10 15:13:29 +0200

    Source versioning: SQL Server source table integration tests

--- integration/sqlserver.tf
+++ integration/sqlserver.tf
@@ -54,5 +54,23 @@ resource "materialize_source_sqlserver" "sqlserver_source_all" {
     database_name = materialize_connection_sqlserver.sqlserver_connection.database_name
   }
 
-  exclude_columns = ["dbo.table3.data", "dbo.table1.about", "dbo.table2.about"]
+  exclude_columns = ["dbo.table3.data", "dbo.table1.about", "dbo.table2.about", "dbo.table5.large_text", "dbo.table5.image_data", "dbo.table5.xml_data", "dbo.table5.json_data", "dbo.table6.description", "dbo.table6.binary_data", "dbo.table10.text_col", "dbo.table10.nvarchar_max"]
+}
+
+# SQL Server Source Table
+resource "materialize_source_table_sqlserver" "sqlserver_table" {
+  name          = "sqlserver_table"
+  schema_name   = materialize_schema.schema.name
+  database_name = materialize_database.database.name
+
+  source {
+    name          = materialize_source_sqlserver.sqlserver_source.name
+    schema_name   = materialize_source_sqlserver.sqlserver_source.schema_name
+    database_name = materialize_source_sqlserver.sqlserver_source.database_name
+  }
+
+  upstream_name        = "table1"
+  upstream_schema_name = "dbo"
+
+  exclude_columns = ["id", "about"]
 }
