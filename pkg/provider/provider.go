10870f48c3c741bdff94eafe57812e07af87cf1b
bobbyiliev <bobby@bobbyiliev.com>
2025-12-12 15:33:48 +0200

    source versioning: register source reference datasource

--- pkg/provider/provider.go
+++ pkg/provider/provider.go
@@ -165,6 +165,7 @@ func Provider(version string) *schema.Provider {
 			"materialize_secret":            datasources.Secret(),
 			"materialize_sink":              datasources.Sink(),
 			"materialize_source":            datasources.Source(),
+			"materialize_source_reference":  datasources.SourceReference(),
 			"materialize_scim_groups":       datasources.SCIMGroups(),
 			"materialize_scim_configs":      datasources.SCIMConfigs(),
 			"materialize_sso_config":        datasources.SSOConfig(),
