7171b3bfbf966bb71f752838afe68332dea6a2ea
bobbyiliev <bobby@bobbyiliev.com>
2026-01-02 12:32:59 +0200

    Source versioning: add missing integration tests

--- integration/source_table.tf
+++ integration/source_table.tf
@@ -38,25 +38,6 @@ resource "materialize_source_table_kafka" "kafka_table" {
   comment = "Kafka source table integration test"
 }
 
-# Postgres Source Table
-resource "materialize_source_table_postgres" "postgres_table" {
-  name          = "postgres_source_table"
-  schema_name   = materialize_schema.schema.name
-  database_name = materialize_database.database.name
-
-  source {
-    name          = materialize_source_postgres.example_source_postgres.name
-    schema_name   = materialize_source_postgres.example_source_postgres.schema_name
-    database_name = materialize_source_postgres.example_source_postgres.database_name
-  }
-
-  upstream_name        = "table1"
-  upstream_schema_name = "public"
-
-  text_columns = ["id"]
-  comment      = "Postgres source table integration test"
-}
-
 # MySQL Source Table
 resource "materialize_source_table_mysql" "mysql_table" {
   name          = "mysql_source_table"
@@ -74,6 +55,25 @@ resource "materialize_source_table_mysql" "mysql_table" {
 
   exclude_columns = ["banned"]
   comment         = "MySQL source table integration test"
+}
+
+# Postgres Source Table
+resource "materialize_source_table_postgres" "postgres_table" {
+  name          = "postgres_source_table"
+  schema_name   = materialize_schema.schema.name
+  database_name = materialize_database.database.name
+
+  source {
+    name          = materialize_source_postgres.example_source_postgres.name
+    schema_name   = materialize_source_postgres.example_source_postgres.schema_name
+    database_name = materialize_source_postgres.example_source_postgres.database_name
+  }
+
+  upstream_name        = "table1"
+  upstream_schema_name = "public"
+
+  text_columns = ["id"]
+  comment      = "Postgres source table integration test"
 }
 
 # SQL Server Source Table
