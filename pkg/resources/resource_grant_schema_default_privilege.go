46079de3b1e1385effdd879ada21f55d511c7a56
bobbyiliev <bobby@bobbyiliev.com>
2026-01-15 17:10:13 +0200

    Refactor: extract grant helpers

--- pkg/resources/resource_grant_schema_default_privilege.go
+++ pkg/resources/resource_grant_schema_default_privilege.go
@@ -3,9 +3,6 @@ package resources
 import (
 	"context"
 
-	"github.com/MaterializeInc/terraform-provider-materialize/pkg/materialize"
-	"github.com/MaterializeInc/terraform-provider-materialize/pkg/utils"
-
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
@@ -35,71 +32,9 @@ func GrantSchemaDefaultPrivilege() *schema.Resource {
 }
 
 func grantSchemaDefaultPrivilegeCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	granteeName := d.Get("grantee_name").(string)
-	targetName := d.Get("target_role_name").(string)
-	privilege := d.Get("privilege").(string)
-
-	metaDb, region, err := utils.GetDBClientFromMeta(meta, d)
-	if err != nil {
-		return diag.FromErr(err)
-	}
-
-	b := materialize.NewDefaultPrivilegeBuilder(metaDb, "SCHEMA", granteeName, targetName, privilege)
-
-	var database string
-	if v, ok := d.GetOk("database_name"); ok && v.(string) != "" {
-		database = v.(string)
-		b.DatabaseName(database)
-	}
-
-	// create resource
-	if err := b.Grant(); err != nil {
-		return diag.FromErr(err)
-	}
-
-	// Query ids
-	gId, err := materialize.RoleId(metaDb, granteeName)
-	if err != nil {
-		return diag.FromErr(err)
-	}
-
-	tId, err := materialize.RoleId(metaDb, targetName)
-	if err != nil {
-		return diag.FromErr(err)
-	}
-
-	var dId string
-	if database != "" {
-		dId, err = materialize.DatabaseId(metaDb, materialize.MaterializeObject{Name: database})
-		if err != nil {
-			return diag.FromErr(err)
-		}
-	}
-
-	key := b.GrantKey(string(region), "SCHEMA", gId, tId, dId, "", privilege)
-	d.SetId(key)
-
-	return grantDefaultPrivilegeRead(ctx, d, meta)
+	return createDefaultPrivilegeGrant(ctx, d, meta, "SCHEMA")
 }
 
 func grantSchemaDefaultPrivilegeDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	granteenName := d.Get("grantee_name").(string)
-	targetName := d.Get("target_role_name").(string)
-	privilege := d.Get("privilege").(string)
-
-	metaDb, _, err := utils.GetDBClientFromMeta(meta, d)
-	if err != nil {
-		return diag.FromErr(err)
-	}
-
-	b := materialize.NewDefaultPrivilegeBuilder(metaDb, "SCHEMA", granteenName, targetName, privilege)
-
-	if v, ok := d.GetOk("database_name"); ok && v.(string) != "" {
-		b.DatabaseName(v.(string))
-	}
-
-	if err := b.Revoke(); err != nil {
-		return diag.FromErr(err)
-	}
-	return nil
+	return revokeDefaultPrivilegeGrant(d, meta, "SCHEMA")
 }
