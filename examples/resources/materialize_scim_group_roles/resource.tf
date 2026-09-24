e064ba092c8112a85ad22d0de3bf07bea93f2a16
justin <justin@materialize.com>
2026-09-24 14:52:11 -0500

    docs: Match SCIM 2.0 groups in role mapping example

--- examples/resources/materialize_scim_group_roles/resource.tf
+++ examples/resources/materialize_scim_group_roles/resource.tf
@@ -10,7 +10,7 @@ resource "materialize_organization_role" "reader" {
 locals {
   analytics_groups = [
     for group in data.materialize_scim_groups.all.groups : group
-    if group.name == "analytics-team" && group.managed_by == "scim"
+    if group.name == "analytics-team" && startswith(group.managed_by, "scim")
   ]
 }
 
